83613d2b8b1240238673f285a38ac040996f9cd9
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-07 16:18:12 -0300

    Update Lampada.java

--- ExercicioLampada/src/Lampada.java
+++ ExercicioLampada/src/Lampada.java
@@ -1,12 +1,12 @@
 
-public class Lampada {
+public class Lampada { //INSTANCIANDO UMA CLASSE
 
-	private boolean ligada;
+	private boolean ligada; //EMCAPSULAMENTO E O TIPO DA VARIAVEL
 	
 	
-	public Lampada(boolean ligada) {
+	public Lampada(boolean ligada) { //CRIAÇÃO DE UM MÉTODO //MÉTODO É EQUIVALENTE A UMA FUNÇÃO, NA DÚVIDA É UM VERBOKKKK
 		
-		this.ligada = ligada;
+		this.ligada = ligada; //FUNÇÃO DO MÉTODO 
 	}
 
 
