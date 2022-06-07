2502514ac7a9f41cac1cdb36f90640cd458b13a7
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-07 16:09:22 -0300

    Update Moto.java

--- Herança/src/Moto.java
+++ Herança/src/Moto.java
@@ -1,5 +1,5 @@
 
-public class Moto  extends Veiculo{
+public class Moto  extends Veiculo{ //SUBCLASSE DA CLASSE PAI "veiculo"  //É NECESSARIO USAR O COMANDO "extends" PARA QUE ELA POSSA SE TORNAR UMA SUBCLASSE
 	
 	public void dargrau() {
 		System.out.println("Chama o Daramkkk");
