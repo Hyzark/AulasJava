72231e9beb459cbc959077d8bee68bc7994da7a7
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-07 16:26:55 -0300

    Update main.java

--- ExercicioLampada/src/main.java
+++ ExercicioLampada/src/main.java
@@ -1,40 +1,41 @@
 import java.util.Scanner;
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //ENTENDENDO O CONCEITO DE MÉTODO
 		
 		
 		
-		Lampada l1 = new Lampada(false);
+		Lampada l1 = new Lampada(false); //TODA VEZ QUE É FEITA UMA INSTANCIA DE UM OBJETO, É NECESSÁRIO QUE EXISTA UM VALOR PRÉ DEFINIDO DE ACORDO COM O PARAMETRO. ISSO OCORRE POR CONTA DO "constructor"
 		
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in); //SCANNER PARA PEGAR AS INFOS
 		
-		System.out.println("A lampada est� " + l1.imprimir());
+		System.out.println("A lampada está " + l1.imprimir()); //CHAMANDO O MÉTODO DE "lampada"
 		System.out.println();
 		
 		
 		System.out.println("deseja ligar a lampada?");;
 		String lol = in.nextLine();
 		
+		// IF PARA PERGUNTAR AO USUÁRIO SE ELE QUER QUE A LAMPADA SEJA LIGADA 
 		if( (lol.equals("sim"))) {
-		l1.ligar();
-		System.out.println("Agora ela est�: " + l1.imprimir());
+		l1.ligar();  //CHAMANDO O MÉTODO
+		System.out.println("Agora ela está: " + l1.imprimir()); //CHAMANDO O MÉTODO
 		System.out.println();
 		System.out.println( "Deseja desligar ela novamente?");
 		String lo = in.nextLine();
-		
+		// SE A CONDIÇÃO FOR TRUE, ELE IRÁ SER DIRECIONADO AO UM NOVO IF, QUE TEM COMO FUNÇÃO, PERGUNTAR SE O USUÁRIO DESEJA DESLIGAR A LAMPADA NOVAMENTE
 	    if( (lo.equals("sim"))) {
-		l1.desligar();
-		System.out.println("Agora ela est�: " + l1.imprimir());
+		l1.desligar();  //CHAMANDO O MÉTODO
+		System.out.println("Agora ela está: " + l1.imprimir()); //CHAMANDO O MÉTODO
 		}
 		
-		}else if( (lol.equals("nao"))) { 
-			l1.ligar();
-			System.out.println("Ent�o ela ir� continuar: " + l1.imprimir());
-		System.out.println("Agora ela est�: " + l1.imprimir());
+		}else if( (lol.equals("nao"))) { //ELSE PARA CASO A CONDIÇÃO DER FALSE
+			l1.ligar(); //CHAMANDO O MÉTODO
+			System.out.println("Então ela irá continuar: " + l1.imprimir()); //CHAMANDO O MÉTODO
+		System.out.println("Agora ela está: " + l1.imprimir());
 	}else if( (lol.equals("nao"))) { 
-		l1.desligar();
-		System.out.println("Ent�o ela ir� continuar: " + l1.imprimir());
+		l1.desligar(); //CHAMANDO O MÉTODO
+		System.out.println("Então ela irá continuar: " + l1.imprimir()); //CHAMANDO O MÉTODO
 		
 		}
 	}
