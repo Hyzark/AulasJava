2e5a7eadd758a58e76f5e75ae323378f8846e4a1
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-07 15:53:23 -0300

    Update ItemCaro.java

--- interface/src/ItemCaro.java
+++ interface/src/ItemCaro.java
@@ -1,5 +1,5 @@
 
-public interface ItemCaro{ //UMA DEFINICAO SIMPLES PARA "interface" SERIA QUE ELA EH UM MOLDE PARA TODAS CLASSES 
+public interface ItemCaro{ //UMA DEFINICAO SIMPLES PARA "interface" SERIA QUE ELA EH UM MOLDE PARA AS OUTRAS CLASSES 
 
 	double getPreco();
 	
