07ea4b92644fc1a013d66c370a3cc93c46b3ccbc
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-13 12:04:30 -0300

    Update and rename List/src/Conta.java to Lista/src/Conta.java

--- Lista/src/Conta.java
+++ Lista/src/Conta.java
@@ -1,15 +1,15 @@
-
+//INSTANCIANDO UMA CLASSE
 public class Conta {
-
+//ATRIBUTOS
 	String conta;
 	double valor;
 	
-	
+	//MÉLETODOS
 	public Conta(String conta, double valor) {
 		this.conta = conta;
 		this.valor = valor;
 	}
-
+//TOSTRING
 	@Override
 	public String toString() {
 		return "Conta [conta=" + conta + ", valor=" + valor + "]";
