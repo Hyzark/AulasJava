a09f7c33a833579930590cb6118468806bd7bcce
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-13 12:11:03 -0300

    Update Main.java

--- ExercicioContaBancaria/src/Main.java
+++ ExercicioContaBancaria/src/Main.java
@@ -1,11 +1,11 @@
-
+//MAIN
 public class Main {
 
 	public static void main(String[] args) {
-	
+	//INSTANCIA DE OBJETOS 
 		ContaCorrente c1 = new ContaCorrente();
 		ContaInvestimento c2 = new ContaInvestimento();
-		
+		//APLICANDO OS MÉTODOS
 		c1.depositar(90);
 		c1.Sacar(90);
 		c1.transferir(90, c2);
@@ -15,7 +15,7 @@ public class Main {
 		c2.depositar(90);
 		c2.Sacar(90);
 		c2.transferir(90, c1);
-		
+		//SYSO PARA O CONSOLE
 		System.out.println("Saldo total: " + c2.CalcularSaldo());
 		
 
