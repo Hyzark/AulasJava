593d16e0426bcb2b643a091d80f8ecb23be731be
Hyzark <102963071+Hyzark@users.noreply.github.com>
2022-06-13 06:06:50 -0300

    Update Main.java

--- EscritaeLeitura/src/Main.java
+++ EscritaeLeitura/src/Main.java
@@ -1,3 +1,4 @@
+//FAZENDO AS IMPORTAÇÕES
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -6,16 +7,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//INSTANCIANDO A CLASSE
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		Scanner in = new Scanner(System.in);
+                //INSTANCIANDO "jogadores"
 		Jogadores bappe = new Jogadores();
-		
+		//INSTANCIANDO UMA LIST DE "jogadores"
 		List<Jogadores> listaDeJogadores = new ArrayList<Jogadores>();
-		
+		//ADICIONANDO A INSTÂNCIA DE JOGADORES NA LIST
 		listaDeJogadores.add(bappe);
 
 		for (int i = 0; i < 3; i++)
@@ -28,7 +30,7 @@ public class Main {
 			System.out.println("Qual o numero da camisa dele?");
 			bappe.setNum(in.nextInt());
 
-
+                        //ESCREVENDO NO TXT
 			try (BufferedWriter escrever = new BufferedWriter(new FileWriter("Jogadores.txt"))) {
 			
 				for (Jogadores fut : listaDeJogadores) {
@@ -36,10 +38,11 @@ public class Main {
 				}
 			}
 		}
+                //LENDO NO CONSOLE
 		try (BufferedReader reader = new BufferedReader(new FileReader("Jogadores.txt"))) {
 			String line;
 			String fut = "";
-
+                        //WHILE PARA FAZER A SEPARAÇÃO DAS LIST NO CONSOLE, BASICAMENTE ESTÉTICA
 			while ((line = reader.readLine()) != null) {
 				fut += line + "\n";
 				System.out.println(fut);
